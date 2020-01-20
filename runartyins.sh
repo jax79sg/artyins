8f23e6e0dd721e871ae98d7b8eb0701f08782b5a
jax79sg <jax79sg@yahoo.com.sg>
2020-01-20 10:06:27 +0800

    update

--- runartyins.sh
+++ runartyins.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir ./logs
 mkdir ./mysql-data
 mkdir ./shareddata
 mkdir ./shareddata/new
 mkdir ./shareddata/processing
 mkdir ./shareddata/success
 mkdir ./shareddata/fail
-docker-compose up --force-recreate --remove-orphans
+docker-compose up --daemon --force-recreate --remove-orphans
